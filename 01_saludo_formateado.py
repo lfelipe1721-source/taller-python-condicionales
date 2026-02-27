059ac72716976c3bb9254bf2cb944f809d488cf2
Luis Felipe M <lfelipe1721@gmail.com>
2026-02-27 11:28:03 -0500

    Ejercicio 1 agregado, saludo formateado

---
+++ 01_saludo_formateado.py
@@ -0,0 +1,10 @@
+# Ejercicio1
+
+# Mostrar: Hola Juan, tienes 20 años y vives en Quito
+
+nombre = input("escribe tu nombre:")
+edad = int(input("escribe tu edad:"))
+ciudad = input("escribe donde vives:")
+
+print (f"Hola{nombre}, tienes {edad} años y vives en {ciudad}")
+
